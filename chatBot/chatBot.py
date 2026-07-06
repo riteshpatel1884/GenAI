b1238ea99dbf92666607269a6068c52eef3b062f
Ritesh Patel <riteshpatel1882004@gmail.com>
2026-07-06 23:26:31 +0530

    add

--- chatBot/chatBot.py
+++ chatBot/chatBot.py
@@ -77,12 +77,18 @@ load_dotenv()
 model = init_chat_model("groq:meta-llama/llama-4-scout-17b-16e-instruct")
 
 print("Choose the AI mood")
-print("Choose 1 for happy/ funny")
-print("Choose 2 for sad")
-print("Choose 3 for angry")
+print("Choose 1 for angry")
+print("Choose 2 for happy/ funny")
+print("Choose 3 for sad")
 
 choice = int(input("Ente the mood of AI"))
 
+if choice == 1:
+    mode = "You are an angry AI agent. You respond aggressively and impatiently"
+if choice == 2:
+    mode = "You are a very funny AI agent. You respond with humor and jokes"
+if choice == 13:
+    mode = "You are a sad AI agent. You respond sadly"
 history = [
     SystemMessage("You are a funny AI")
 
