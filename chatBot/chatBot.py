5e678fccd13dd632742566d5b48a697accf0156c
Ritesh Patel <riteshpatel1882004@gmail.com>
2026-07-06 23:32:58 +0530

    ADD NEW

--- chatBot/chatBot.py
+++ chatBot/chatBot.py
@@ -68,34 +68,35 @@
 
 
 
-from dotenv import load_dotenv
-from langchain.chat_models import init_chat_model
-from langchain_core.messages import AIMessage, SystemMessage, HumanMessage
+# from dotenv import load_dotenv
+# from langchain.chat_models import init_chat_model
+# from langchain_core.messages import AIMessage, SystemMessage, HumanMessage
 
-load_dotenv() 
+# load_dotenv() 
 
-model = init_chat_model("groq:meta-llama/llama-4-scout-17b-16e-instruct")
+# model = init_chat_model("groq:meta-llama/llama-4-scout-17b-16e-instruct")
 
-print("Choose the AI mood")
-print("Choose 1 for angry")
-print("Choose 2 for happy/ funny")
-print("Choose 3 for sad")
+# print("Choose the AI mood")
+# print("Choose 1 for angry")
+# print("Choose 2 for happy/ funny")
+# print("Choose 3 for sad")
 
-choice = int(input("Ente the mood of AI"))
+# choice = int(input("Ente the mood of AI"))
 
-if choice == 1:
-    mode = "You are an angry AI agent. You respond aggressively and impatiently"
-if choice == 2:
-    mode = "You are a very funny AI agent. You respond with humor and jokes"
-if choice == 13:
-    mode = "You are a sad AI agent. You respond sadly"
-history = [
-    SystemMessage(content=mode)
+# if choice == 1:
+#     mode = "You are an angry AI agent. You respond aggressively and impatiently"
+# if choice == 2:
+#     mode = "You are a very funny AI agent. You respond with humor and jokes"
+# if choice == 13:
+#     mode = "You are a sad AI agent. You respond sadly"
+# history = [
+#     SystemMessage(content=mode)
 
-]
-while True:
-    prompt = input("You: ")
-    history.append(HumanMessage(content=prompt))  #HumanMessage It is an object 
-    response = model.invoke(history)
-    history.append(AIMessage(content=response.content))
-    print("AI:", response.content)
+# ]
+# while True: 
+#     prompt = input("You: ")
+#     history.append(HumanMessage(content=prompt))  #HumanMessage It is an object 
+#     response = model.invoke(history)
+#     history.append(AIMessage(content=response.content))
+#     print("AI:", response.content)
+
