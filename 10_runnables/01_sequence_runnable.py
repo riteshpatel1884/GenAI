657577013c133a69a3087f509178129ae4245788
Ritesh Patel <riteshpatel1882004@gmail.com>
2026-07-14 08:45:47 +0530

    add

--- 10_runnables/01_sequence_runnable.py
+++ 10_runnables/01_sequence_runnable.py
@@ -1,3 +1,38 @@
+# from dotenv import load_dotenv
+# load_dotenv()
+
+# from langchain_groq import ChatGroq
+# from langchain_core.prompts import ChatPromptTemplate
+# from langchain_core.output_parsers import StrOutputParser
+
+# # 1. prompt templete
+# prompt = ChatPromptTemplate.from_template(
+#     "Explain {topic} in simple word"
+# )
+
+# # 2. model
+# model = ChatGroq(
+#     model="llama-3.3-70b-versatile"
+# )
+
+# # 3. structured output parser
+# parser = StrOutputParser()
+
+
+# # step ny step manual flow
+# formatted_prompt = prompt.format_messages(topic="Machine Learning")
+
+# #call model manually
+# response = model.invoke(formatted_prompt)
+
+# # parse the output manually
+# final_output = parser.parse(response.content)
+
+# print(final_output)
+
+
+
+
 from dotenv import load_dotenv
 load_dotenv()
 
@@ -5,12 +40,11 @@ from langchain_groq import ChatGroq
 from langchain_core.prompts import ChatPromptTemplate
 from langchain_core.output_parsers import StrOutputParser
 
-# 1. prompt templete
 prompt = ChatPromptTemplate.from_template(
     "Explain {topic} in simple word"
 )
 
-# 2. model
+
 model = ChatGroq(
     model="llama-3.3-70b-versatile"
 )
